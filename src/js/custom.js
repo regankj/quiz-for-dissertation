096c90be8b83c55e9aa18ce1adc4e25dd763943a
Regan Jones <jonesrk6@cardiff.ac.uk>
2020-07-13 12:22:41 +0100

    csv filescan now be parsed

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,32 +1,59 @@
-import * as d3 from 'd3';
+const d3 = require('d3');
+
 
 // Function to make sure Privacy Statement is accepted
-document.getElementById('continue').addEventListener('click', function(){
-  const checkbox = document.getElementById('check');
-  var text = document.getElementById('checkError');
-  const btn = document.getElementById('continue');
-  const infoForm = document.getElementById('infoForm');
-  if (checkbox.checked == false){
-    text.style.display = "block";
-    text.style.color = "red";
-  } else {
-    text.style.display = "none";
-    btn.type = "submit";
-    infoForm.action = "quiz.html";
-  }
-})
+const contBtn = document.getElementById('continue');
+if (contBtn){
+  contBtn.addEventListener('click', function(){
+    const checkbox = document.getElementById('check');
+    var text = document.getElementById('checkError');
+    const btn = document.getElementById('continue');
+    const infoForm = document.getElementById('infoForm');
+    if (checkbox.checked == false){
+      text.style.display = "block";
+      text.style.color = "red";
+    } else {
+      text.style.display = "none";
+      btn.type = "submit";
+      infoForm.action = "quiz.html";
+    };
+  });
+};
 
-// reading the csv into the webpage
-const qform = document.getElementById('questions');
+
+
+function addRow(){
+  const qform = document.getElementById('questions');
+  const newRow = document.createElement('div');
+  newRow.className = "form-row my-md-3";
+  qform.appendChild(newRow);
+  const col = document.createElement('div');
+  col.className = "col-md-6";
+  newRow.appendChild(col);
+};
+
+/*
+loading in a csv file
+code taken and adapted from https://stackoverflow.com/questions/29259938/how-to-load-csv-file-to-use-with-d3
+accessed 12-07-20
+*/
 d3.csv("Sample-Data/sample.csv").then(function(data){
-  console.log(data[1]);
+  var i = 0;
+  data.forEach(function(d){
+    i++;
+    d3.select("#questions").append("label").text(i + ". " + d.Question);
+    addRow();
+    d3.select("#questions").append("label").text(d.Option1);
+    addRow();
+    d3.select("#questions").append("label").text(d.Option2);
+    addRow();
+    d3.select("#questions").append("label").text(d.Option3);
+    addRow();
+    d3.select("#questions").append("label").text(d.Option4);
+    addRow();
+    d3.select("#questions").append("label").text(d.Option5);
+    addRow();
+  })
 });
 
-// connecting to the database
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "regan",
-  password: "mypassword",
-  database: "dbase"
-});
+// end of referenced code
