2bb62310654b14359eea3ce07a412c6debf8bcd1
Regan Jones <jonesrk6@cardiff.ac.uk>
2020-10-16 16:25:53 +0100

    Done some type of visualisation

--- src/js/custom.js
+++ src/js/custom.js
@@ -811,9 +811,15 @@ function showSlide(n, currentSlideID, slideNo){
     var rand = Math.random();
     savePrimingTest(uid, rand);
     if (rand >= 0.5 && rand < 0.75){
-      qform.prepend("Please read the following article from The Guardian before answering this question https://www.theguardian.com/world/2020/jul/12/face-masks-shops-will-not-be-mandatory-england-gove-suggests");
+      qform.prepend("Please read the following article from The Guardian before answering this question");
+      var link = document.createElement('a');
+      link.innerHTML = "https://www.theguardian.com/world/2020/jul/12/face-masks-shops-will-not-be-mandatory-england-gove-suggests";
+      qform.prepend(link)
     } else if (rand >= 0.75){
-      qform.prepend("Please read the following article from The Guardian before answering this question https://www.theguardian.com/world/2020/may/04/scientists-disagree-over-face-masks-effect-on-covid-19");
+      qform.prepend("Please read the following article from The Guardian before answering this question");
+      var link = document.createElement('a');
+      link.innerHTML = "https://www.theguardian.com/world/2020/may/04/scientists-disagree-over-face-masks-effect-on-covid-19";
+      qform.prepend(link);
 
     }
 
@@ -844,6 +850,7 @@ function showSlide(n, currentSlideID, slideNo){
 
       createDropdown(qform);
       addRow(qform);
+      createChart();
     }
   }
 }
@@ -852,6 +859,7 @@ function showSlide(n, currentSlideID, slideNo){
 function createDropdown(area){
   var drop = document.createElement("select");
   drop.class = "form-control";
+  drop.id = "selector";
   area.appendChild(drop);
 
   for (var d = 1; d <= 61; d++){
@@ -864,12 +872,122 @@ function createDropdown(area){
 }
 
 function createChart(){
-  for (var c = 0; c < 61; c++){
-    var qNum = "question" + (c+1);
-    document.getElementById(qNum).addEventListener('click', function(){
-      console.log("sound");
-    })
-  }
+  var vis = document.createElement("div");
+  vis.id = "vis";
+  qform.appendChild(vis);
+
+  var width = 500;
+  var height = 500;
+
+  var maxValue = 100;
+
+  var margin = {
+    top: 20,
+    left: 30,
+    right: 30,
+    bottom: 30
+  };
+
+  var svg = d3.select('#vis')
+            .append("svg")
+            .attr('width', width)
+            .attr('height', height)
+            .append("g")
+            .attr("transform", "translate(" + margin.top + "," + margin.left + ")");
+
+  width = width - margin.left - margin.right;
+  height = height - margin.top - margin.bottom;
+
+  var yscale = d3.scaleLinear().range([height, 0]).domain([0, maxValue]);
+
+  var xscale = d3.scaleBand().range([0, width]).padding(0.1);
+
+  var duration = 1000;
+
+  var xaxis = d3.axisBottom(xscale);
+  var yaxis = d3.axisLeft(yscale);
+
+  svg.append('g')
+      .attr('transform', 'translate(0' + (height) + ')')
+      .attr('class', 'x axis');
+
+  svg.append('g').attr('class', 'y axis');
+
+  var selector = document.getElementById("selector");
+  selector.addEventListener('change', function(){
+    var theLbl = selector.options[selector.selectedIndex].text;
+    var vNum = theLbl.replace("Question ", "values");
+    // put actual question here
+    xscale.domain(d3.range(values[vNum].length));
+    yscale.domain([0, maxValue]);
+
+    var bars = svg.selectAll(".bar").data(values[vNum]);
+
+    bars.enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('fill', 'red')
+        .attr('width', xscale.bandwidth())
+        .attr('height', 0)
+        .attr('y', height)
+        .merge(bars)
+        .transition()
+        .duration(duration)
+        .attr("height", function(d, i){ return height - yscale(d); })
+        .attr("y", function(d, i){ return yscale(d); })
+        .attr("width", xscale.bandwidth())
+        .attr("x", function(d, i){ return xscale(i); });
+
+    bars.exit()
+        .transition()
+        .duration(duration)
+        .attr('height', 0)
+        .attr('y', height)
+        .remove();
+
+    var labels = svg.selectAll('.label')
+        .data(values[vNum]);
+
+    labels
+        .enter()
+        .append('text')
+            .attr('class', 'label')
+            .attr('opacity', 0)
+            .attr('y', height)
+            .attr('fill', 'white')
+        .merge(labels)
+            .transition()
+            .duration(duration)
+            .attr('opacity', 1)
+            .attr('x', function(d, i) {
+                return xscale(i) + 1;
+            })
+            .attr('y', function(d){
+                return yscale(d) + 20;
+            })
+            .text(function(d){
+                return d;
+            });
+
+    labels
+        .exit()
+        .transition()
+        .duration(duration)
+        .attr('y', height)
+        .attr('opacity', 0)
+        .remove();
+
+    svg.select('.x.axis')
+        .transition()
+        .duration(duration)
+        .call(xaxis);
+
+    svg.select('.y.axis')
+        .transition()
+        .duration(duration)
+        .call(yaxis);
+
+  });
 }
 
 // reads in scoring csv file and adds the original BSAS results into arrays
