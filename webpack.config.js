9351acea83ca1cdcaf963b435fe52ab69248fa42
Regan Jones <jonesrk6@cardiff.ac.uk>
2020-10-25 14:36:38 +0000

    some complicated stuff

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
         test: /\.(scss)$/,
         use: [
           {
